520b4eeecfb9d1bcc190fdc87797f2c812c0e312
Kandasamy Chokkalingam <samchoks@hotmail.com>
2021-01-18 16:15:46 -0500

    Updates

---
+++ component---src-pages-events-js-09e3f19e234bfa6306ed.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{UV5A:function(e,n,a){"use strict";a.r(n),a.d(n,"default",(function(){return o}));var t=a("q1tI"),l=a.n(t),c=a("U5tq"),r=a("JwsL");a("37Ea");function o(){return l.a.createElement("div",{className:"container main-container h-screen mx-auto font-serif debug-screens"},l.a.createElement(c.a,null),l.a.createElement("div",{className:"flex flex-col min-h-full longpage pb-nav md:flex-row"}),l.a.createElement(r.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-events-js-09e3f19e234bfa6306ed.js.map
